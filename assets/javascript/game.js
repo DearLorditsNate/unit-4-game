92bf2ccd2e91c3bfb52e7348c1632cee611ac701
Nate Micinski <nmicinski@gmail.com>
2019-01-05 17:09:48 -0800

    refactor random number assignment, won't allow duplicate values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,38 +20,39 @@ $(document).ready(function() {
     // Crystal number assignment
     $("#red-btn").attr("value", randomCrystalNumber());
     redBtnValue = $("#red-btn").attr("value");
-    console.log("Red Button: " + redBtnValue);
+    randomValues.push(redBtnValue);
 
     $("#blue-btn").attr("value", randomCrystalNumber());
     blueBtnValue = $("#blue-btn").attr("value");
-    console.log("First time blue button: " + blueBtnValue)
-    if (blueBtnValue === redBtnValue) {
-        $("#blue-btn").attr("value", randomCrystalNumber());
-        blueBtnValue = $("#blue-btn").attr("value");
+    for (var i = 0; i < randomValues.length; i++) {
+        while (blueBtnValue === randomValues[i]) {
+            $("#blue-btn").attr("value", randomCrystalNumber());
+            blueBtnValue = $("#blue-btn").attr("value");
+        };
     };
-    console.log("Second time blue button: " + blueBtnValue);
+    randomValues.push(blueBtnValue);
 
     $("#yellow-btn").attr("value", randomCrystalNumber());
     yellowBtnValue = $("#yellow-btn").attr("value");
-    console.log("First time yellow button: " + yellowBtnValue)
-    if (yellowBtnValue === redBtnValue || yellowBtnValue === blueBtnValue) {
-        $("#yellow-btn").attr("value", randomCrystalNumber());
-        yellowBtnValue = $("#yellow-btn").attr("value");
+    for (var i = 0; i < randomValues.length; i++) {
+        while (yellowBtnValue === randomValues[i]) {
+            $("#yellow-btn").attr("value", randomCrystalNumber());
+            yellowBtnValue = $("#yellow-btn").attr("value");
+        };
     };
-    console.log("Second time yellow button: " + yellowBtnValue);
+    randomValues.push(yellowBtnValue);
 
     $("#green-btn").attr("value", randomCrystalNumber());
     greenBtnValue = $("#green-btn").attr("value");
-    console.log("First time green button: " + greenBtnValue)
-    if (greenBtnValue === redBtnValue || greenBtnValue === blueBtnValue || greenBtnValue === yellowBtnValue) {
-        $("#green-btn").attr("value", randomCrystalNumber());
-        greenBtnValue = $("#green-btn").attr("value");
+    for (var i = 0; i < randomValues.length; i++) {
+        while (greenBtnValue === randomValues[i]) {
+            $("#green-btn").attr("value", randomCrystalNumber());
+            greenBtnValue = $("#green-btn").attr("value");
+        };
     };
-    console.log("Second time green button: " + greenBtnValue);
+    randomValues.push(greenBtnValue);
 
-
-    // Crystal number assignment
-    
+    console.log("randomValues array: " + randomValues);
     
 
     /*
@@ -66,12 +67,6 @@ $(document).ready(function() {
 
     function randomTargetNumber() {
         return Math.floor(Math.random() * (121 - 19) + 19);
-    };
-
-    function checkForDupes(id, value) {
-        if (id ) {
-            randomCrystalNumber();
-        };
     };
 
 });
